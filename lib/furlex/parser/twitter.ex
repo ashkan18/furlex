c19c237f24ad58c5463b9e31081e11cb68ec40e0
Ashkan Nasseri <ashkan.nasseri@gmail.com>
2021-01-05 10:08:47 -0500

    Update dependencies and limit twitter tags

--- lib/furlex/parser/twitter.ex
+++ lib/furlex/parser/twitter.ex
@@ -3,14 +3,9 @@ defmodule Furlex.Parser.Twitter do
 
   alias Furlex.Parser
 
+
   @tags ~w(
-    twitter:card twitter:site twitter:domain twitter:url twitter:site:id
-    twitter:creator twitter:creator:id twitter:description twitter:title
-    twitter:image twitter:image:alt twitter:player twitter:player:width
-    twitter:player:height twitter:player:stream twitter:app:name:iphone
-    twitter:app:id:iphone twitter:app:url:iphone twitter:app:name:ipad
-    twitter:app:id:ipad twitter:app:url:ipad twitter:app:name:googleplay
-    twitter:app:url:googleplay twitter:app:id:googleplay
+    twitter:site twitter:description twitter:title twitter:image
   )
 
   @spec parse(String.t) :: {:ok, Map.t}
